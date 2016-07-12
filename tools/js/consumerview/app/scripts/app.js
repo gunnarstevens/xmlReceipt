367be6ba670f6e4e8571b41ad47b9ff3982728df
Gunnar Stevens <gunnar.stevens@uni-siegen.de>
2016-07-12 18:18:38 +0200

    add subproject consumerview

---
+++ tools/js/consumerview/app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name consumerviewApp
+ * @description
+ * # consumerviewApp
+ *
+ * Main module of the application.
+ */
+var receiptApp = angular
+  .module('consumerviewApp', [
+    'ngAnimate', 'xml'
+  ]);
+
